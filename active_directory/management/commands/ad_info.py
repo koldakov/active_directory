8c04dbf63fd97c9de579c6d0314e5536481bb859
Ivan <jonnykold@mail.ru>
2021-02-09 21:01:12 +0700

    Added simple management command

---
+++ active_directory/management/commands/ad_info.py
@@ -0,0 +1,53 @@
+from django.core.management.base import BaseCommand
+from active_directory.utils.active_directory import get_users_info_ad
+
+
+class Command(BaseCommand):
+    help = 'Get information from active_directory by given parameters'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-u',
+            '--users',
+            nargs='+',
+            help='Get users information from active directory'
+        )
+
+        parser.add_argument(
+            '-a',
+            '--attributes',
+            nargs='*',
+            help='What attributes to search in active directory'
+        )
+
+        parser.add_argument(
+            '-lu',
+            '--login-username',
+            nargs=1,
+            help='Active directory username login'
+        )
+
+        parser.add_argument(
+            '-lp',
+            '--login-password',
+            nargs=1,
+            help='Active directory password login'
+        )
+
+    def handle(self, *args, **options):
+        users = options.get('users') or None
+        attributes = options.get('attributes') or '*'
+        login_username = options.get('login_username')[0] if options.get('login_username') else None
+        login_password = options.get('login_password')[0] if options.get('login_password') else None
+
+        if users:
+            users_info = get_users_info_ad(
+                login_username=login_username, login_password=login_password,
+                users=users, attributes=attributes
+            )
+            if users_info:
+                print(users_info)
+            else:
+                return 'Users not found'
+
+        return '-h/--help'
