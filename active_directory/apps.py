789ac66c4d475b6029126306243d16890f6b9802
Ivan <jonnykold@mail.ru>
2021-03-30 23:22:15 +0700

    Validate active directory username on save & added verbose_name for app

--- active_directory/apps.py
+++ active_directory/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ActiveDirectoryConfig(AppConfig):
     name = 'active_directory'
+    verbose_name = _("Active directory")
--- active_directory/forms.py
+++ active_directory/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Settings
+from django.utils.translation import gettext as _
 
 
 class SettingsForm(forms.ModelForm):
@@ -10,3 +11,14 @@ class SettingsForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput(),
         }
+
+    def clean(self):
+        cleaned_data = super(SettingsForm, self).clean()
+        username = cleaned_data.get('username')
+        separators = ('@', '\\')
+
+        if username.startswith(separators) or username.endswith(separators):
+            raise forms.ValidationError(_(f'Username should not starts or ends with {" or ".join(separators)}.'))
+
+        if not any(separator in username for separator in separators):
+            raise forms.ValidationError(_(f'Username should contain {" or ".join(separators)}.'))
